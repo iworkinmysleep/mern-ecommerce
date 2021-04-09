f81847a831ea5b5f91bc4897c91a9c199bab26cb
Jason Hinton <jason_hinton@att.net>
2021-04-09 01:07:55 -0400

    added user update profile functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,10 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
-import CartScreen from './screens/CartScreen'
+import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const App = () => {
 	return (
@@ -15,10 +16,11 @@ const App = () => {
 			<Header />
 			<main className="py-3">
 				<Container>
-					<Route path="/login" component={LoginScreen}/>
-					<Route path="/register" component={RegisterScreen}/>
-					<Route path="/product/:id" component={ProductScreen}/>
-					<Route path='/cart/:id?' component={CartScreen}/>
+					<Route path="/login" component={LoginScreen} />
+					<Route path="/register" component={RegisterScreen} />
+					<Route path="/profile" component={ProfileScreen} />
+					<Route path="/product/:id" component={ProductScreen} />
+					<Route path="/cart/:id?" component={CartScreen} />
 					<Route path="/" component={HomeScreen} exact />
 				</Container>
 			</main>
--- frontend/src/screens/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen.js
@@ -85,7 +85,7 @@ const RegisterScreen = ({ location, history }) => {
 
 			<Row className="py-3">
 				<Col>
-					Have Accout?{" "}
+					Have Account?{" "}
 					<Link to={redirect ? `/login?redirect=${redirect}` : "/login"}>
 						Login
 					</Link>
