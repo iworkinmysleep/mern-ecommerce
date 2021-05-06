34b1e5b03b3d2377e4a1f55bbee52b890e05d58b
Jason Hinton <jason_hinton@att.net>
2021-05-05 23:54:07 -0400

    added top products carousel

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,12 +6,17 @@ import {
 	deleteProduct,
 	createProduct,
 	updateProduct,
-	createProductReview
+	createProductReview,
+	getTopProducts,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct);
-router.route('/:id/reviews').post(protect, createProductReview)
-
+router.get("/top", getTopProducts);
+router
+	.route("/:id")
+	.get(getProductById)
+	.delete(protect, admin, deleteProduct)
+	.put(protect, admin, updateProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 export default router;
